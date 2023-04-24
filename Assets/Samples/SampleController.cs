f3a0c7d5c67ae29029187b46ad65a016d9845180
cukr2675 <cukr2675@gmail.com>
2023-04-24 21:41:19 +0900

    Add samples asmdef

--- Assets/Samples/SampleController.cs
+++ Assets/Samples/SampleController.cs
@@ -4,112 +4,114 @@ using UnityEngine;
 
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
-using GrippedWebGLTemplate;
 
-public class SampleController : MonoBehaviour
+namespace GrippedWebGLTemplate.Samples
 {
-    [SerializeField]
-    private Camera mainCamera = null;
-
-    [SerializeField]
-    private RawImage image = null;
-
-    [SerializeField]
-    private Tilemap tilemap = null;
-
-    [SerializeField]
-    private Tile tile = null;
-
-    [Header("AspectRatio")]
-    [SerializeField] private Toggle keepAspectRatioToggle = null;
-    [SerializeField] private Dropdown aspectRatioDropdown = null;
-
-    [Header("Zoom")]
-    [SerializeField] private Dropdown zoomDropdown = null;
-
-    [Space]
-    [SerializeField] private Button submitButton = null;
-    [SerializeField] private Text fpsText = null;
-
-    private int frameCount;
-    private float lastTime;
-    private int lastHeight;
-
-    private RenderTexture renderTexture;
-    private RectTransform imageTransform;
-
-    private const int tileRange = 63;
-
-    private void Start()
+    public class SampleController : MonoBehaviour
     {
-        //NewRenderTexture(new Vector2Int(1920, 1080), 1);
+        [SerializeField]
+        private Camera mainCamera = null;
 
-        tilemap.BoxFill(Vector3Int.one * -tileRange, tile, -tileRange, -tileRange, tileRange, tileRange);
-        tilemap.BoxFill(Vector3Int.one * tileRange, tile, -tileRange, -tileRange, tileRange, tileRange);
+        [SerializeField]
+        private RawImage image = null;
 
-        submitButton.onClick.AddListener(() =>
+        [SerializeField]
+        private Tilemap tilemap = null;
+
+        [SerializeField]
+        private Tile tile = null;
+
+        [Header("AspectRatio")]
+        [SerializeField] private Toggle keepAspectRatioToggle = null;
+        [SerializeField] private Dropdown aspectRatioDropdown = null;
+
+        [Header("Zoom")]
+        [SerializeField] private Dropdown zoomDropdown = null;
+
+        [Space]
+        [SerializeField] private Button submitButton = null;
+        [SerializeField] private Text fpsText = null;
+
+        private int frameCount;
+        private float lastTime;
+        private int lastHeight;
+
+        private RenderTexture renderTexture;
+        private RectTransform imageTransform;
+
+        private const int tileRange = 63;
+
+        private void Start()
         {
-            var keepAspectRatio = keepAspectRatioToggle.isOn;
-            if (keepAspectRatio)
+            //NewRenderTexture(new Vector2Int(1920, 1080), 1);
+
+            tilemap.BoxFill(Vector3Int.one * -tileRange, tile, -tileRange, -tileRange, tileRange, tileRange);
+            tilemap.BoxFill(Vector3Int.one * tileRange, tile, -tileRange, -tileRange, tileRange, tileRange);
+
+            submitButton.onClick.AddListener(() =>
             {
-                var option = aspectRatioDropdown.options[aspectRatioDropdown.value];
-                var sizeTexts = option.text.Split(':');
-                var width = float.Parse(sizeTexts[0]);
-                var height = float.Parse(sizeTexts[1]);
-                GrippedWebGL.LockAspectRatio(width / height);
-                Debug.Log($"Lock aspect ratio {width} x {height}");
+                var keepAspectRatio = keepAspectRatioToggle.isOn;
+                if (keepAspectRatio)
+                {
+                    var option = aspectRatioDropdown.options[aspectRatioDropdown.value];
+                    var sizeTexts = option.text.Split(':');
+                    var width = float.Parse(sizeTexts[0]);
+                    var height = float.Parse(sizeTexts[1]);
+                    GrippedWebGL.LockAspectRatio(width / height);
+                    Debug.Log($"Lock aspect ratio {width} x {height}");
+                }
+                else
+                {
+                    GrippedWebGL.FreeAspectRatio();
+                    Debug.Log($"Free aspect ratio");
+                }
+            });
+
+            frameCount = 0;
+            lastTime = Time.time;
+        }
+
+        private void Update()
+        {
+            frameCount++;
+            var now = Time.time;
+            if (now >= lastTime + 1f)
+            {
+                fpsText.text = frameCount.ToString();
+                frameCount = 0;
+                lastTime = now;
+            }
+
+            var screenSize = new Vector2Int(Screen.width, Screen.height);
+            if (screenSize.y != lastHeight)
+            {
+                NewRenderTexture(screenSize);
+                lastHeight = screenSize.y;
+            }
+        }
+
+        private void NewRenderTexture(Vector2Int screenSize, int zoom = -1)
+        {
+            if (zoom == -1) { zoom = int.Parse(zoomDropdown.options[zoomDropdown.value].text); }
+
+            if (image != null && image.enabled)
+            {
+                if (renderTexture != null) { RenderTexture.ReleaseTemporary(renderTexture); }
+                renderTexture = RenderTexture.GetTemporary(screenSize.x, screenSize.y);
+                renderTexture.autoGenerateMips = false;
+                renderTexture.filterMode = FilterMode.Point;
+                mainCamera.targetTexture = renderTexture;
+                image.texture = renderTexture;
+                imageTransform = image.rectTransform;
+                imageTransform.sizeDelta = screenSize;
+
+                imageTransform.localScale = new Vector3(zoom, zoom, 1f);
+                mainCamera.orthographicSize = screenSize.y / tile.sprite.pixelsPerUnit * .5f;
             }
             else
             {
-                GrippedWebGL.FreeAspectRatio();
-                Debug.Log($"Free aspect ratio");
+                mainCamera.orthographicSize = screenSize.y / tile.sprite.pixelsPerUnit * .5f / zoom;
             }
-        });
-
-        frameCount = 0;
-        lastTime = Time.time;
-    }
-
-    private void Update()
-    {
-        frameCount++;
-        var now = Time.time;
-        if (now >= lastTime + 1f)
-        {
-            fpsText.text = frameCount.ToString();
-            frameCount = 0;
-            lastTime = now;
-        }
-
-        var screenSize = new Vector2Int(Screen.width, Screen.height);
-        if (screenSize.y != lastHeight)
-        {
-            NewRenderTexture(screenSize);
-            lastHeight = screenSize.y;
-        }
-    }
-
-    private void NewRenderTexture(Vector2Int screenSize, int zoom = -1)
-    {
-        if (zoom == -1) { zoom = int.Parse(zoomDropdown.options[zoomDropdown.value].text); }
-
-        if (image != null && image.enabled)
-        {
-            if (renderTexture != null) { RenderTexture.ReleaseTemporary(renderTexture); }
-            renderTexture = RenderTexture.GetTemporary(screenSize.x, screenSize.y);
-            renderTexture.autoGenerateMips = false;
-            renderTexture.filterMode = FilterMode.Point;
-            mainCamera.targetTexture = renderTexture;
-            image.texture = renderTexture;
-            imageTransform = image.rectTransform;
-            imageTransform.sizeDelta = screenSize;
-
-            imageTransform.localScale = new Vector3(zoom, zoom, 1f);
-            mainCamera.orthographicSize = screenSize.y / tile.sprite.pixelsPerUnit * .5f;
-        }
-        else
-        {
-            mainCamera.orthographicSize = screenSize.y / tile.sprite.pixelsPerUnit * .5f / zoom;
         }
     }
 }
